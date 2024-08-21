040f2f4402bfcac668dfdcc3ccf5cbb6986392a5
HENNING KÖNITZ <koenitz92@gmx.de>
2024-08-21 13:11:08 +0200

    add the columns to the return

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,8 @@ function App() {
   const [gameStarted, setGameStarted] = useState(false);
 
   // to the new variable newDeck gets assigned the shuffled deck from 'getShuffledDeck' --> easier to read, inspect, debug
-  const handlePlayButtonClick = () => {
+  const handlePlayButton = () => {
+    setGameStarted(true);
     const newDeck = getShuffledDeck();
     setDeck(newDeck);
 
@@ -25,8 +26,6 @@ function App() {
     }
 
     setColumns(newColumns);
-
-    setGameStarted(true);
   };
 
   return (
@@ -34,17 +33,34 @@ function App() {
       <h1>Solitaire</h1>
       {/* if game was not started yet, you see the button, otherwise the shuffled deck appears */}
       {!gameStarted ? (
-        <button onClick={handlePlayButtonClick}>Play</button>
+        <button onClick={handlePlayButton}>Play</button>
       ) : (
-        <div>
-          {/* 'map' iterates over each card in the deck array and transforms in into JSX elements for rendering */}
-          {/* parameters: card: the current element in the deck array | index: index of the current element for identification */}
-          {/* key={index}: idenifier for each element | */}
-          {deck.map((card) => (
-            <div key={card.id}>
-              {card.rank} of {card.suit}
-            </div>
-          ))}
+        <div className="game-area">
+          <div className="deck-section">
+            <h2>Shuffled Deck</h2>
+            {/* 'map' iterates over each card in the deck array and transforms in into JSX elements for rendering */}
+            {/* parameter 'card': the current element in the deck array */}
+            {/* key={card.id}: idenifier for each element */}
+            {deck.map((card) => (
+              <div key={card.id}>
+                {card.rank} of {card.suit}
+              </div>
+            ))}
+          </div>
+          <div className="columns-section">
+            {/* column: the current column | columnIndex: individual column index  */}
+            {/* for each column, a div element is created */}
+            {/* column.map((card): iterate over the individual cards in the column */}
+            {columns.map((column, columnIndex) => (
+              <div key={columnIndex} className="column">
+                {column.map((card) => (
+                  <div key={card.id}>
+                    {card.rank} of {card.suit}
+                  </div>
+                ))}
+              </div>
+            ))}
+          </div>
         </div>
       )}
     </div>
